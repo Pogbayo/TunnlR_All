cdd7029e3ab7a4167702a5c5551d57925f8948bc
Pogbayo <adebayooluwasegun335@gmail.com>
2026-02-10 13:33:34 +0100

    Added a desgn time factory for the database

---
+++ TunnlR.Infrastructure/DesignTime/TunnelDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using TunnlR.RelayServer.Persistence;
+
+namespace TunnlR.Infrastructure.DesignTime
+{
+    public class TunnelDbContextFactory : IDesignTimeDbContextFactory<TunnelDbContext>
+    {
+        public TunnelDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                ?? "Server=localhost;Database=TunnlRDb;User Id=tunnlr_user;Password=StrongPassword123!;Encrypt=True;TrustServerCertificate=True;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<TunnelDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new TunnelDbContext(optionsBuilder.Options);
+        }
+    }
+}
