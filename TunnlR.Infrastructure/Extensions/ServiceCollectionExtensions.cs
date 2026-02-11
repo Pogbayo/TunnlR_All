a7f181fb6b5d669de67d0365be480f8667109960
Pogbayo <adebayooluwasegun335@gmail.com>
2026-02-11 04:25:25 +0100

    #

--- TunnlR.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ TunnlR.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -24,7 +24,7 @@ namespace TunnlR.Infrastructure.Extensions
                 .AddEntityFrameworkStores<TunnelDbContext>()
                 .AddDefaultTokenProviders();
 
-            //I just built a ci/cd pipeline to test
+            //I just built a ci/cd pipeline to test bb
             //Token
             services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
